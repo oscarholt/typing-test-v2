622f342d61355dc4be6eb91f398f7fbdf7a1871f
oscarholt <38869989+oscarholt@users.noreply.github.com>
2021-12-23 14:32:08 +1300

    Setup UI and components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Preview from "./Preview.js";
+import Speed from "./Speed.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5 mb-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <Preview></Preview>
+          <textarea
+            className="form-control mb-3"
+            placeHolder="Start typing..."
+          ></textarea>
+          <Speed></Speed>
+          <div className="text-right">
+            <button className="btn btn-light">Restart</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Preview.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default (props) => {
+  return (
+    <div className="border rounded p-3 mb-4">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi deleniti
+      sapiente dolore impedit modi? Temporibus laborum, incidunt veniam, id
+      aspernatur voluptate quam minus culpa aliquid doloribus eaque fugit
+      ducimus reprehenderit.
+    </div>
+  );
+};
