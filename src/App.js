685bf6fb7547cf7805f4ef350692aea994651779
oscarholt <38869989+oscarholt@users.noreply.github.com>
2021-12-23 14:42:50 +1300

    fixes to App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
+import React, { Component } from "react";
 import Preview from "./Preview.js";
 import Speed from "./Speed.js";
 
-function App() {
-  return (
-    <div className="container mt-5 mb-5">
-      <div className="row">
-        <div className="col-md-6 offset-md-3">
-          <Preview></Preview>
-          <textarea
-            className="form-control mb-3"
-            placeHolder="Start typing..."
-          ></textarea>
-          <Speed></Speed>
-          <div className="text-right">
-            <button className="btn btn-light">Restart</button>
+const initialState = {
+  text: "test",
+  userInput: "",
+};
+
+class App extends Component {
+  state = initialState;
+
+  onRestart = () => {
+    this.setState(initialState);
+  };
+
+  onUserInputChange = (e) => {
+    const v = e.target.value;
+    this.setState({
+      userInput: v,
+    });
+  };
+
+  render() {
+    return (
+      <div className="container mt-5 mb-5">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">
+            <Preview></Preview>
+            <textarea
+              value={this.state.userInput}
+              onChange={this.onUserInputChange}
+              className="form-control mb-3"
+              placeholder="Start typing..."
+            ></textarea>
+            <Speed></Speed>
+            <div className="text-right">
+              <button className="btn btn-light" onClick={this.onRestart}>
+                Restart
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
